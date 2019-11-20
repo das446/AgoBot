ccc354cb484ea24790e4c7982be8b831a3c1604e
David Saks <das446@drexel.edu>
2019-11-20 11:49:06 -0500

    added skeleton for boardgame file

---
+++ boardgame.py
@@ -0,0 +1,10 @@
+class BoardGame:
+    def __init__(self,name):
+        self.name = name
+
+def ReadCsvFileIntoListOfGames():
+    return []
+
+def RandomBoardGames(filters):
+    boardGames = ReadCsvFileIntoListOfGames(fileName)
+    return N games that match all of the filters

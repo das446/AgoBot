637c8b67a960e42ca6a39e75a6ab71aec82d55f1
Void-Void <qan24@drexel.edu>
2019-11-22 21:06:53 -0500

    Wrote class for boardgames, and function to search for boardgames by name

--- boardgame.py
+++ boardgame.py
@@ -1,10 +1,37 @@
+import csv
+
 class BoardGame:
-    def __init__(self,name):
-        self.name = name
+    def __init__(self):
+        #Initialize game data
+        self.name = list()
+        self.shelf = list()
+        self.sticker = list()
+        self.opened = list()
+        self.player_num = list()
+        self.type = list()
+        self.duration = list() 
+        self.something = list()     #I dont know what this column is, it just says LGBN/Popular/Chelsea/Classic/Played/Expansion
+        self.year = list()
+        self.condition = list() 
 
-def ReadCsvFileIntoListOfGames():
-    return []
+        #Look through the Excel file to add game datas into lists
+        with open("BoardGames.csv") as csvDataFile: 
+            csvReader = csv.reader(csvDataFile)
+            for row in csvReader:
+                self.name.append(row[0])
+                self.shelf.append(row[1])
+                self.sticker.append(row[2])
+                self.opened.append(row[3])
+                self.player_num.append(row[4])
+                self.type.append(row[5])
+                self.duration.append(row[6])
+                self.something.append(row[7])
+                self.year.append(row[8])
+                self.condition.append(row[9])
 
-def RandomBoardGames(filters):
-    boardGames = ReadCsvFileIntoListOfGames(fileName)
-    return N games that match all of the filters
+    #Take in a search parameter, read the file to get all the game information, then put all the game names into boardgames variable. Compare game name with filter to see if any game contains the filter, then print it out
+    def RandomBoardGames(self, filters):
+        filters = str(filters)
+        for i in self.name:
+            if filters in str(i).lower():
+                print(i)

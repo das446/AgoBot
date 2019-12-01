d6ddd2b77370e4c4d4a1e32e3565bac22f3c93a3
Void-Void <qan24@drexel.edu>
2019-12-01 13:47:07 -0500

    Edited how BoardGame class works, now each game is a separate object. SearchGame and ReadBoardGame are now separate function instead of method of BoardGame class.

--- boardgame.py
+++ boardgame.py
@@ -1,37 +1,42 @@
 import csv
 
+games = []           #List to contain board game objects
+
 class BoardGame:
-    def __init__(self):
+    def __init__(self, name, shelf, sticker, opened, player_num, game_type, duration, notes, year, condition):
         #Initialize game data
-        self.name = list()
-        self.shelf = list()
-        self.sticker = list()
-        self.opened = list()
-        self.player_num = list()
-        self.type = list()
-        self.duration = list() 
-        self.something = list()     #I dont know what this column is, it just says LGBN/Popular/Chelsea/Classic/Played/Expansion
-        self.year = list()
-        self.condition = list() 
+        self.name = name
+        self.shelf = shelf
+        self.sticker = sticker
+        self.opened = opened
+        self.player_num = player_num
+        self.type = game_type
+        self.duration = duration
+        self.notes = notes
+        self.year = year
+        self.condition = condition
 
-        #Look through the Excel file to add game datas into lists
-        with open("BoardGames.csv") as csvDataFile: 
-            csvReader = csv.reader(csvDataFile)
-            for row in csvReader:
-                self.name.append(row[0])
-                self.shelf.append(row[1])
-                self.sticker.append(row[2])
-                self.opened.append(row[3])
-                self.player_num.append(row[4])
-                self.type.append(row[5])
-                self.duration.append(row[6])
-                self.something.append(row[7])
-                self.year.append(row[8])
-                self.condition.append(row[9])
+#Read Excel file and create an object for each new game+ in the list
+def ReadGames():
+    with open('BoardGames.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            new_game = BoardGame(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9])
+            games.append(new_game)
+            
 
-    #Take in a search parameter, read the file to get all the game information, then put all the game names into boardgames variable. Compare game name with filter to see if any game contains the filter, then print it out
-    def RandomBoardGames(self, filters):
-        filters = str(filters)
-        for i in self.name:
-            if filters in str(i).lower():
-                print(i)
+#Take in search parameters and look through list of board game items to see if any matches
+def SearchBoardGames(filters):
+    filters = str(filters)
+    if len(filter) < 2:                                #If search parameter only has 1 or fewer letter, search for games that starts with that letter
+        for i in games:
+            if i.name[0].lower() == filters.lower():
+                print(i.name)
+    else:    
+        for i in games:
+            if filters.lower() in i.name.lower():
+                print(i.name) 
+
+ReadGames()
+filter = input()
+SearchBoardGames(filter)

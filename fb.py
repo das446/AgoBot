b06e88205f866f43140e0655f62bffa0a6cc504d
David Saks <das446@drexel.edu>
2019-12-02 10:29:17 -0500

    remove fb.swp

--- fb.py
+++ fb.py
@@ -4,6 +4,8 @@ from HTMLParser import HTMLParser
 from bs4 import BeautifulSoup as bs
 import facebook
 
-token = open(os.path.join("files", "fb_key.txt")).read()
+token = open(os.path.join("files","fb_key.txt")).read()
 
 graph = facebook.GraphAPI.(access_token=token, version="5.0")
+
+
